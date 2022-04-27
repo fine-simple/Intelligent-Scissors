dd20c1aa2582ca83ec701f43eceaa0262eb5534c
aymanmostafa11 <ayman.mostafa58@hotmail.com>
2022-04-27 07:21:00 +0200

    Added Simple interaction with gui
    check the TODO

--- src/IntelligentScissors/MainForm.cs
+++ src/IntelligentScissors/MainForm.cs
@@ -10,6 +10,9 @@ namespace IntelligentScissors
 {
     public partial class MainForm : Form
     {
+        public bool EnableLasso = false;
+        Point anchorPoint, lastPoint, currentPoint;
+        Pen pen;
         public MainForm()
         {
             InitializeComponent();
@@ -29,18 +32,59 @@ namespace IntelligentScissors
             }
             txtWidth.Text = ImageOperations.GetWidth(ImageMatrix).ToString();
             txtHeight.Text = ImageOperations.GetHeight(ImageMatrix).ToString();
+
+            Graph.Init(ImageMatrix, 4);
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            pen = new Pen(Color.FromArgb(255, 255, 0, 0));
+
+            Point controlLoc = this.PointToScreen(pictureBox1.Location); // location of the pitcture to screen(not form)
+            /* last and current points co-ordinates are relative to picture however drawing is relative to
+               the form window
+               TODO: Map the co-ordinates to display the line on the picture
+             */
+            e.Graphics.DrawLine(pen, (float)lastPoint.X, (float)lastPoint.Y, (float)currentPoint.X, (float)currentPoint.Y);
         }
 
         private void btnGaussSmooth_Click(object sender, EventArgs e)
         {
-            //double sigma = double.Parse(txtGaussSigma.Text);
-            //int maskSize = (int)nudMaskSize.Value ;
-            //ImageMatrix = ImageOperations.GaussianFilter1D(ImageMatrix, maskSize, sigma);
-            //ImageOperations.DisplayImage(ImageMatrix, pictureBox2);
-            Graph.Init(ImageMatrix, 4);
+            
         }
 
-       
-       
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        { 
+            if (!EnableLasso) // first click on picture
+            {
+                anchorPoint = new Point();
+                anchorPoint.X = e.Location.X;
+                anchorPoint.Y = e.Location.Y;
+                lastPoint = anchorPoint;
+
+                currentPoint = new Point();
+                currentPoint.X = e.Location.X;
+                currentPoint.Y = e.Location.Y;
+                currentPoint = anchorPoint;
+
+                mousePos.Text = anchorPoint.ToString();
+
+                EnableLasso = true;
+
+            }
+            else
+            {
+                lastPoint.X = currentPoint.X;
+                lastPoint.Y = currentPoint.Y;
+
+                currentPoint.X = e.Location.X;
+                currentPoint.Y = e.Location.Y;
+
+                mousePos.Text = currentPoint.ToString();
+            }
+
+            // used to force the form to re-draw (aka call MainForm_Paint)
+            this.Invalidate();
+        }
     }
 }
