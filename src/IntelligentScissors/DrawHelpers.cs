3fb9cda19efc3a114ce9b21f61404f77e2c72fdf
aymanmostafa11 <ayman.mostafa58@hotmail.com>
2022-05-09 14:37:55 +0200

    Added eucledian distance

--- src/IntelligentScissors/DrawHelpers.cs
+++ src/IntelligentScissors/DrawHelpers.cs
@@ -65,5 +65,9 @@ namespace IntelligentScissors
             }    
             return scaledP;
         }
+        private static double eucledianDistance(Point p1, Point p2)
+        {
+            return Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y));
+        }
     }
 }
