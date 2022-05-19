bd3e037206cd0acccfdf0b54ae1bfe5c143e9b44
Ahmed Tawfik <at200000000@gmail.com>
2022-05-19 20:57:07 +0200

    Added Save to file after cropping

--- src/IntelligentScissors/CroppedPreviewForm.Designer.cs
+++ src/IntelligentScissors/CroppedPreviewForm.Designer.cs
@@ -30,24 +30,37 @@ namespace IntelligentScissors
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(694, 438);
+            this.pictureBox1.Size = new System.Drawing.Size(694, 392);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.saveBtn.Location = new System.Drawing.Point(281, 398);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(127, 28);
+            this.saveBtn.TabIndex = 1;
+            this.saveBtn.Text = "Save To File";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
             // 
             // CroppedPreviewForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(694, 438);
+            this.Controls.Add(this.saveBtn);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -64,5 +77,6 @@ namespace IntelligentScissors
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button saveBtn;
     }
 }
--- src/IntelligentScissors/CroppedPreviewForm.cs
+++ src/IntelligentScissors/CroppedPreviewForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Drawing.Imaging;
 using System.Windows.Forms;
 
 namespace IntelligentScissors
@@ -22,6 +23,54 @@ namespace IntelligentScissors
         {
             pictureBox1.Image = cropped;
             pictureBox1.Invalidate();
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.AddExtension = true;
+            dialog.Filter = "JPEG|*.jpg" +
+                "|GIF|*.gif" +
+                "|PNG|*.png" +
+                "|BMP|*.bmp" +
+                "|ICON|*.ico" +
+                "|TIFF|*.tiff" +
+                "|EMF|*.emf";
+            dialog.DefaultExt = "jpg";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                ImageFormat format;
+                switch (dialog.FilterIndex)
+                {
+                    case 0:
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case 1:
+                        format = ImageFormat.Png;
+                        break;
+                    case 2:
+                        format = ImageFormat.Bmp;
+                        break;
+                    case 3:
+                        format = ImageFormat.Icon;
+                        break;
+                    case 4:
+                        format = ImageFormat.Tiff;
+                        break;
+                    case 5:
+                        format = ImageFormat.Emf;
+                        break;
+                    case 6:
+                        format = ImageFormat.Gif;
+                        break;
+                    default:
+                        format = ImageFormat.Jpeg;
+                        break;
+                }
+                Console.WriteLine(dialog.FilterIndex);
+                pictureBox1.Image.Save(dialog.FileName, format);
+                MessageBox.Show("Image Saved Successfully");
+            }
         }
     }
 }
