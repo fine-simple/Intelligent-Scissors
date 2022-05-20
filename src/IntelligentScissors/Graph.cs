1963622bab5f03bd34d12f3df2c4fdb3940f85a4
aymanmostafa11 <ayman.mostafa58@hotmail.com>
2022-05-20 16:32:29 +0200

    Graph construction documentation

--- src/IntelligentScissors/Graph.cs
+++ src/IntelligentScissors/Graph.cs
@@ -28,12 +28,12 @@ namespace IntelligentScissors
 
             vertexCount = imageWidth * imageHeight;
             
-            adj = new List<KeyValuePair<int, double>>[vertexCount];
+            adj = new List<KeyValuePair<int, double>>[vertexCount] // O(N^2)
 
             constructGraph(ImageMatrix);
         }
 
-        private static void constructGraph(RGBPixel[,] ImageMatrix)
+        private static void constructGraph(RGBPixel[,] ImageMatrix) // O(N^2)
         {
             for (int row = 0; row < imageHeight; row++)
             {
@@ -48,9 +48,70 @@ namespace IntelligentScissors
 
 
         #region Helper Functions
+        
+        
+
+        /* the function adds the energy of current pixel to the neighbour pixel
+         * and vice versa to establish the 4 way connectivity
+         */
+        private static void addEnergyFor(int row, int col, Vector2D pixelEnergies) // O(1)
+        {
+            double rightPixelEnergy = pixelEnergies.X == 0? WEIGHT_UPPER_LIMIT: 1 / pixelEnergies.X;
+            double botPixelEnergy = pixelEnergies.Y == 0 ? WEIGHT_UPPER_LIMIT : 1 / pixelEnergies.Y;
+
+            int mainPixelIndex = convert2DIndexTo1D(row, col);
+
+            if (adj[mainPixelIndex] == null)
+                adj[mainPixelIndex] = new List<KeyValuePair<int, double>>();
+
+            //add to right pixel
+            if (validIndex(row, col + 1))
+            {
+                int rightPixelIndex = convert2DIndexTo1D(row, col + 1);
+                addEnergyToNeighbour(mainPixelIndex, rightPixelIndex, rightPixelEnergy);
+            }
+
+            //add to bottom pixel
+            if (validIndex(row + 1, col))
+            {
+                int botPixelIndex = convert2DIndexTo1D(row + 1, col);
+                addEnergyToNeighbour(mainPixelIndex, botPixelIndex, botPixelEnergy);
+            }
+            
+        }
+        private static void addEnergyToNeighbour(int mainPixelIndex, int neighbourIndex, double energy) // O(1)
+        {
+            if (adj[neighbourIndex] == null)
+                adj[neighbourIndex] = new List<KeyValuePair<int, double>>();
+
+            adj[neighbourIndex].Add(new KeyValuePair<int, double>(mainPixelIndex, energy));
+
+            adj[mainPixelIndex].Add(new KeyValuePair<int, double>(neighbourIndex, energy));
+        }
+
+        public static int convert2DIndexTo1D(int row, int col)
+        {
+            return row * imageWidth + col;
+        }
+
+        public static Point convert1DIndexTo2D(int index)
+        {
+            int col = index % imageWidth;
+            int row = (index-col) / imageWidth;
+
+            return new Point(col, row);
+        }
+
+        public static bool validIndex(int row, int col)
+        {
+            bool lowerBound = (row >= 0 && col >= 0);
+            bool upperBound = (row < imageHeight && col < imageWidth);
+            return lowerBound && upperBound;
+        }
+
         public static double getNeighbourWeight(int mainRow, int mainCol, Neighbour neighbourType)
         {
-            
+
             Point neighbour = getNeighbourLocation(mainRow, mainCol, neighbourType);
 
             int indx = convert2DIndexTo1D(mainRow, mainCol);
@@ -80,7 +141,7 @@ namespace IntelligentScissors
                 case Neighbour.Bot:
                     targetRow++;
                     break;
-                   
+
             }
             if (validIndex(targetRow, targetCol))
                 return new Point(targetCol, targetRow);
@@ -112,65 +173,6 @@ namespace IntelligentScissors
                 throw new ArgumentException("Current location doesn't have requested neighbour type!");
         }
 
-        
-
-        /* the function adds the energy of current pixel to the neighbour pixel
-         * and vice versa to establish the 4 way connectivity
-         */
-        private static void addEnergyFor(int row, int col, Vector2D pixelEnergies)
-        {
-            double rightPixelEnergy = pixelEnergies.X == 0? WEIGHT_UPPER_LIMIT: 1 / pixelEnergies.X;
-            double botPixelEnergy = pixelEnergies.Y == 0 ? WEIGHT_UPPER_LIMIT : 1 / pixelEnergies.Y;
-
-            int mainPixelIndex = convert2DIndexTo1D(row, col);
-
-            if (adj[mainPixelIndex] == null)
-                adj[mainPixelIndex] = new List<KeyValuePair<int, double>>();
-
-            //add to right pixel
-            if (validIndex(row, col + 1))
-            {
-                int rightPixelIndex = convert2DIndexTo1D(row, col + 1);
-                addEnergyToNeighbour(mainPixelIndex, rightPixelIndex, rightPixelEnergy);
-            }
-
-            //add to bottom pixel
-            if (validIndex(row + 1, col))
-            {
-                int botPixelIndex = convert2DIndexTo1D(row + 1, col);
-                addEnergyToNeighbour(mainPixelIndex, botPixelIndex, botPixelEnergy);
-            }
-            
-        }
-        private static void addEnergyToNeighbour(int mainPixelIndex, int neighbourIndex, double energy)
-        {
-            if (adj[neighbourIndex] == null)
-                adj[neighbourIndex] = new List<KeyValuePair<int, double>>();
-
-            adj[neighbourIndex].Add(new KeyValuePair<int, double>(mainPixelIndex, energy));
-
-            adj[mainPixelIndex].Add(new KeyValuePair<int, double>(neighbourIndex, energy));
-        }
-
-        public static int convert2DIndexTo1D(int row, int col)
-        {
-            return row * imageWidth + col;
-        }
-
-        public static Point convert1DIndexTo2D(int index)
-        {
-            int col = index % imageWidth;
-            int row = (index-col) / imageWidth;
-
-            return new Point(col, row);
-        }
-
-        public static bool validIndex(int row, int col)
-        {
-            bool lowerBound = (row >= 0 && col >= 0);
-            bool upperBound = (row < imageHeight && col < imageWidth);
-            return lowerBound && upperBound;
-        }
 
         #endregion
 
